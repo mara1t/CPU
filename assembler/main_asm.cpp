a17ddf1b7d16e04184ed750c9d20f5c6f63f39e6
mara1t <88665544+mara1t@users.noreply.github.com>
2021-12-16 22:30:56 +0300

    Update main_asm.cpp
    jmp+op+call

--- assembler/main_asm.cpp
+++ assembler/main_asm.cpp
@@ -1,4 +1,4 @@
-#include "asm_header.h"
+#include "asm.h"
 
 
 int main()
@@ -7,11 +7,13 @@ int main()
     FILE* transfile = fopen("translated_in.txt", "wb");
 
     if (inputfile == NULL || transfile == NULL)
-        printf("input.txt not open = %d\ntranslated_file.txt not open = %d\n", inputfile == NULL, transfile == NULL);
-
+        printf("input.txt not open = %d\n\
+                translated_file.txt not open = %d\n", inputfile == NULL, transfile == NULL);
+    
     else if (assemble (inputfile, transfile) != 0)
         printf("ERROR in assembler\n");
 
+    
     fclose(transfile);
     fclose(inputfile);
     return 0;
